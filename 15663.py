7601f48346ee07076496aef89c2f1e42206e46ca
김서인 <79834222+seoin0110@users.noreply.github.com>
2023-06-06 10:48:28 +0900

    N과 M (9)

--- 15663.py
+++ 15663.py
@@ -1,5 +1,5 @@
 n, m = map(int, input().split())
-arr = map(int, input().split())
+arr = list(map(int, input().split()))
 arr.sort()
 ans = []
 check = [0 for i in range(n)]
@@ -9,18 +9,12 @@ def func(k):
     if k == m:
         ans.append(tmp.copy())
         return
-    if check[0] == 0:
-        check[0] = 1
-        tmp[k] = arr[0]
-        func(k+1)
-        check[0] = 0
-    for i in range(1,n):
+    for i in range(0,n):
         if check[i] == 0:
             check[i] = 1
             tmp[k] = arr[i]
             func(k+1)
             check[i] = 0
-
 func(0)
 
 ans.sort()
