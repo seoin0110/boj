df63c4f87604cf839a9e786def3bdab89eb94522
김서인 <79834222+seoin0110@users.noreply.github.com>
2026-04-21 08:28:47 +0900

    [Silver II] Title: 타일링, Time: 140 ms, Memory: 110404 KB -BaekjoonHub

---
+++ PyPy3/Silver/1793. 타일링/타일링.py
@@ -0,0 +1,18 @@
+while True:
+    try:
+        n = int(input())
+        dp = [0 for i in range(n+1)]
+        if n == 0:
+            print(1)
+        elif n== 1:
+            print(1)
+        elif n == 2:
+            print(3)
+        else:
+            dp[1] = 1
+            dp[2] = 3
+            for i in range(3,n+1):
+                dp[i] = dp[i-1] + dp[i-2] * 2
+            print(dp[n])
+    except:
+        break
