078659bba718f08775778e7bf556420e0b7523b6
김서인 <79834222+seoin0110@users.noreply.github.com>
2026-04-21 08:22:11 +0900

    [Silver IV] Title: 동가수열 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/Silver/25184. 동가수열 구하기/동가수열 구하기.cc
+++ C++17/Silver/25184. 동가수열 구하기/동가수열 구하기.cc
@@ -1,46 +1,30 @@
 #include <iostream>
+#include <algorithm>
+#include <string>
 #include <vector>
+#include <cmath>
+#include <functional>
+#include <climits>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <cstring>
+#define ll long long
 using namespace std;
 
 int main() {
     ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0), cout.tie(0);
+    ll n;
+    cin >> n;
+    for (ll i = 1; i <= n / 2; i++) {
 
-    int N;
-    cin >> N;
-
-    int half = N / 2;
-    vector<int> groupA, groupB, result;
-
-    // 그룹 A: 큰 수들
-    for (int i = half + 1; i <= N; ++i) {
-        groupA.push_back(i);
+        cout << i + n/2<< " " << i << " ";
     }
-
-    // 그룹 B: 작은 수들
-    for (int i = 1; i <= half; ++i) {
-        groupB.push_back(i);
+    if (n % 2 == 1) {
+        cout << n;
     }
-
-    // 그룹 A와 그룹 B의 원소를 번갈아 가며 결과 수열 생성
-    int indexA = 0, indexB = 0;
-    while (indexA < groupA.size() || indexB < groupB.size()) {
-        if (indexA < groupA.size()) {
-            result.push_back(groupA[indexA++]);
-        }
-        if (indexB < groupB.size()) {
-            result.push_back(groupB[indexB++]);
-        }
-    }
-
-    // 결과 수열 출력
-    for (int i = 0; i < result.size(); ++i) {
-        cout << result[i];
-        if (i != result.size() - 1) {
-            cout << " ";
-        }
-    }
-    cout << endl;
-
     return 0;
 }
