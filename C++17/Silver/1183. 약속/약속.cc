35cb72d4b939c0ae8b93f8dbbd1a8a30ef5f7888
김서인 <79834222+seoin0110@users.noreply.github.com>
2026-04-21 08:22:22 +0900

    [Silver II] Title: 약속, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/Silver/1183. 약속/약속.cc
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<cmath>
+#include<queue>
+#include<string>
+#include<cstring>
+#include<stack>
+#include<map>
+#include<set>
+#include<deque>
+#include<functional>
+
+using ll = long long;
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    ll n;
+    cin >> n;
+    vector<ll>v(n);
+    for (ll i = 0; i < n; i++) {
+        ll a, b;
+        cin >> a >> b;
+        v[i] = a - b;
+    }
+    sort(v.begin(), v.end());
+    if (n % 2 == 1) {
+        cout << 1;
+    }
+    else {
+        cout << v[n / 2] - v[n / 2 - 1] + 1;
+    }
+    return 0;
+}
