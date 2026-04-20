5d1f2bc0b6031dcdc74875a986569c61536ffe60
김서인 <79834222+seoin0110@users.noreply.github.com>
2026-04-21 08:21:22 +0900

    [Bronze V] Title: Plane, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/Bronze/8370. Plane/Plane.cc
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<cmath>
+#include<queue>
+#include<string>
+#include<cstring>
+#include<stack>
+#include<map>
+#include<set>
+#include<deque>
+#include<functional>
+
+using ll = long long;
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    ll a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << a * b + c * d;
+    return 0;
+}
