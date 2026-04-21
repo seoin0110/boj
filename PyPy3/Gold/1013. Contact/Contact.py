1e8b981829829b68216c5ae388285e5642b04d71
김서인 <79834222+seoin0110@users.noreply.github.com>
2026-04-21 22:38:22 +0900

    [Gold V] Title: Contact, Time: 304 ms, Memory: 117472 KB -BaekjoonHub

---
+++ PyPy3/Gold/1013. Contact/Contact.py
@@ -0,0 +1,13 @@
+import re
+
+n = int(input())
+for i in range(n):
+    s = input()
+    ans = 0
+    if re.compile('((100+1+)|(01))+').fullmatch(s):
+        ans = 1
+    if ans:
+        print("YES")
+    else:
+        print("NO")
+    
