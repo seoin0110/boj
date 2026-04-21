eb89f8b59406ef549d31f5ecf8371514027e8ab2
김서인 <79834222+seoin0110@users.noreply.github.com>
2026-04-21 22:48:49 +0900

    [Gold I] Title: 수열과 쿼리 37, Time: 52 ms, Memory: 33272 KB -BaekjoonHub

---
+++ C++17/Gold/18436. 수열과 쿼리 37/수열과 쿼리 37.cc
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <functional>
+#include <climits>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <cstring>
+using ll = long long;
+using namespace std;
+//ll arr[100000];
+ll seg[4000000];
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+	ll n, q;
+	cin >> n;
+	ll base = 1;
+	for (base = 1; base < n; base *= 2);
+	for (ll i = 0; i < n; i++) {
+		cin >> seg[base + i];
+		seg[base + i] %= 2;
+	}
+
+	for (ll i = base - 1; i > 0; i--)
+		seg[i] = seg[i * 2] + seg[i * 2 + 1];
+	cin >> q;
+	while (q--) {
+		ll a, b, c;
+		cin >> a >> b >> c;
+		if (a == 1) {
+			b = base + b - 1;
+			seg[b] = c%2;
+			b /= 2;
+			while (b > 0) {
+				seg[b] = seg[b * 2]+ seg[b * 2+1];
+				b /= 2;
+			}
+		}
+		else {
+			ll ans = 0;
+			ll d = c - b + 1;
+			b = base + b - 1;
+			c = base + c - 1;
+			while (b <= c) {
+				if (b % 2 == 1) {
+					ans += seg[b];
+					b++;
+				}
+				if (c % 2 == 0) {
+					ans += seg[c];
+					c--;
+				}
+				b /= 2;
+				c /= 2;
+			}
+			if(a == 3)
+				cout << ans << '\n';
+			if (a == 2) {
+				cout << d - ans << '\n';
+			}
+
+		}
+		
+
+
+	}
+	return 0;
+}
