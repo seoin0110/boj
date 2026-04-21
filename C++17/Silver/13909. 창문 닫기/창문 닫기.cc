704520fe7dd9307d8eeaf4e135b7dfbafdebdd77
김서인 <79834222+seoin0110@users.noreply.github.com>
2026-04-21 22:34:21 +0900

    [Silver V] Title: 창문 닫기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/Silver/13909. 창문 닫기/창문 닫기.cc
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<cmath>
+#include<queue>
+#include<string>
+#include<cstring>
+#include<stack>
+#include<map>
+#include<set>
+#include<deque>
+#include<functional>
+
+using ll = long long;
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    
+    ll n;
+    cin >> n;
+    ll i = 0;
+    for (; i * i <= n; i++);
+    cout <<i-1;
+    return 0;
+}
