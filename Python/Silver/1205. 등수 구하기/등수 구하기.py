ada5256d819b4269ffa9c96d68467f22dc23cf98
김서인 <79834222+seoin0110@users.noreply.github.com>
2026-04-21 22:33:25 +0900

    [Silver IV] Title: 등수 구하기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ Python/Silver/1205. 등수 구하기/등수 구하기.py
@@ -0,0 +1,17 @@
+a, b, c = map(int,input().split())
+if a >= 1:
+    arr = list(map(int,input().split()))
+idx = 0
+flag = 0
+start_idx = -1
+while idx<a and arr[idx] >= b:
+    if arr[idx] == b and flag == 0:
+        flag = 1
+        start_idx = idx
+    idx+=1
+if idx >= c:
+    print(-1)
+elif start_idx >= 0:
+    print(start_idx+1)
+else:
+    print(idx+1)
