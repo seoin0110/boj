c13e10001634175f76b6ae7ba8395aeccbb6efd6
김서인 <79834222+seoin0110@users.noreply.github.com>
2026-04-21 22:47:59 +0900

    [Gold V] Title: 트리와 쿼리, Time: 72 ms, Memory: 12876 KB -BaekjoonHub

---
+++ C++17/Gold/15681. 트리와 쿼리/트리와 쿼리.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <functional>
+#include <climits>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <cstring>
+#define ll long long
+using namespace std;
+
+
+ll dp[100001];
+vector<ll>v[100001];
+
+void dfs(ll a, ll par) {
+    dp[a] = 1;
+    for (ll i = 0; i < v[a].size(); i++) {
+        if (par == v[a][i])continue;
+        dfs(v[a][i], a);
+        dp[a] += dp[v[a][i]];
+    }
+    return;
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+
+    ll n,r,q;
+    cin >> n >> r >> q;
+    for (ll i = 0; i < n - 1; i++) {
+        ll a, b;
+        cin >> a >> b;
+        v[a].push_back(b);
+        v[b].push_back(a);
+    }
+    dfs(r,r);
+
+    for (ll i = 0; i < q; i++) {
+        ll a;
+        cin >> a;
+        cout << dp[a] << '\n';
+    }
+
+    return 0;
+}
