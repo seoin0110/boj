50512a1687f941bfc35caf494fbd4868b3149514
김서인 <79834222+seoin0110@users.noreply.github.com>
2026-04-21 22:35:04 +0900

    [Silver I] Title: 계단, Time: 12 ms, Memory: 3584 KB, Score: 150 point -BaekjoonHub

---
+++ C++17/Silver/21600. 계단/계단.cc
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<cmath>
+#include<queue>
+#include<string>
+#include<cstring>
+#include<stack>
+#include<map>
+#include<set>
+#include<deque>
+#include<functional>
+
+using ll = long long;
+using namespace std;
+ll dp[100001];
+int main() {
+	cin.tie(0), cout.tie(0);
+	ios_base::sync_with_stdio(0);
+	ll n;
+	cin >> n;
+	vector<ll>v(n);
+	for (ll i = 0; i < n; i++)
+		cin >> v[i];
+	dp[0] = 1;
+	ll ans = 1;
+	for (ll i = 1; i < n; i++) {
+		dp[i] = min(v[i], dp[i - 1] + 1);
+		ans = max(ans, dp[i]);
+	}
+	cout << ans;
+	return 0;
+}
