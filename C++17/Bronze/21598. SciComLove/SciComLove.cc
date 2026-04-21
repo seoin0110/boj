2e8e1b3353e8a6aa4aabc7a94ce66f60f8837114
김서인 <79834222+seoin0110@users.noreply.github.com>
2026-04-21 22:51:30 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ C++17/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<cmath>
+#include<queue>
+#include<string>
+#include<cstring>
+#include<stack>
+#include<map>
+#include<set>
+#include<deque>
+#include<functional>
+
+using ll = long long;
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll n;
+    cin >> n;
+    while (n--) {
+        cout << "SciComLove\n";
+    }
+    return 0;
+
+}
