cd3dd249511f14c1e9aee98978b37ca86217583d
김서인 <79834222+seoin0110@users.noreply.github.com>
2026-04-21 22:40:27 +0900

    [Bronze V] Title: Metronome, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/Bronze/27389. Metronome/Metronome.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using ll = long long;
+using namespace std;
+
+int main() {
+	cin.tie(0), cout.tie(0);
+	ios_base::sync_with_stdio(0);
+	cout.precision(2);
+	cout << fixed;
+	double a;
+	cin >> a;
+	cout << a / 4;
+	return 0;
+}
