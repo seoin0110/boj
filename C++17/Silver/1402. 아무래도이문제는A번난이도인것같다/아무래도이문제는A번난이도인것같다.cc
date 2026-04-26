bc0048a02fbda8ac1eb9fcd1ad7157c93fb013df
김서인 <79834222+seoin0110@users.noreply.github.com>
2026-04-26 17:59:39 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ C++17/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.cc
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+using namespace std;
+using ll = long long;
+
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+
+    ll t;
+    cin >> t;
+    for (int i = 0; i < t;i++) {
+        ll a;
+        ll b;
+        cin >> a >> b;
+        cout << "yes\n";
+    }
+
+    return 0;
+}
