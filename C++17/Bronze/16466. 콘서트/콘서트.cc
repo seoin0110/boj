32ee435017b484ecf5ddf30f1124dfe0804b3f12
김서인 <79834222+seoin0110@users.noreply.github.com>
2026-04-26 17:40:15 +0900

    [Bronze I] Title: 콘서트, Time: 152 ms, Memory: 22252 KB -BaekjoonHub

---
+++ C++17/Bronze/16466. 콘서트/콘서트.cc
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<cmath>
+#include<queue>
+#include<string>
+#include<cstring>
+#include<stack>
+#include<map>
+#include<set>
+#include<deque>
+#include<functional>
+
+using ll = long long;
+using namespace std;
+#define inf 1000000007
+int main() {
+    cin.tie(0), cout.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    ll n;
+    cin >> n;
+    vector<ll>v(n);
+    priority_queue<ll,vector<ll>,greater<> >pq;
+    for (ll i = 0; i < n; i++) {
+        ll a;
+        cin >> a;
+        pq.push(a);
+    }
+    ll ans = 1;
+    while ((pq.top() == ans)&& !pq.empty()) {
+        ans++;
+        pq.pop();
+    }
+
+    cout << ans << '\n';
+    return 0;
+
+}
+
