35e614c758138d858ec761c5d04ad9c2e904f7d1
김서인 <79834222+seoin0110@users.noreply.github.com>
2026-04-26 18:02:28 +0900

    [Bronze I] Title: 적어도 대부분의 배수, Time: 32 ms, Memory: 2016 KB -BaekjoonHub

--- C++17/Bronze/1145. 적어도 대부분의 배수/적어도 대부분의 배수.cc
+++ C++17/Bronze/1145. 적어도 대부분의 배수/적어도 대부분의 배수.cc
@@ -1,34 +1,38 @@
-#include<iostream>
-#include<algorithm>
-#include<vector>
-#include<string>
-
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
 using namespace std;
 using ll = long long;
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0), cout.tie(0);
-    
-    int n[5];
-    int min2 = 0;
-    int min3 = 1000000;
-    for (int i = 0;i < 5;i++)
-        cin >> n[i];
-    min2 = min({ n[0],n[1],n[2],n[3],n[4] });
-    for (int i = 0; i < 3; i++) {
-        for (int j = i + 1;j < 4;j++) {
-            for (int k = j + 1;k < 5;k++) {
-                for (int l = min2; l < 1000000;l++) {
-                    if (l<min3&&l % n[i] == 0 && l % n[j] == 0 && l % n[k] == 0)
-                    {
-                        min3 = l;
-                        break;
-                    }
-                }
-            }
-        }
-    }
 
-    cout << min3<<"\n";
-    return 0;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0), cout.tie(0);
+
+	int n[5];
+	int a[10];
+	int mm;
+	int l = 0;
+	for (int i = 0; i < 5; i++)
+		cin >> n[i];
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = i + 1; j < 5; j++)
+		{
+			for (int k = j + 1; k < 5; k++)
+			{
+				mm = 0;
+				for (int b = max({n[i], n[j], n[k]}); b <= 1000000; b++) {
+					if (b% n[i] == 0 && b%n[j] == 0 && b%n[k] == 0) {
+						mm = b;
+						break;
+					}
+
+				}
+				a[l] = mm;
+				l++;
+			}
+		}
+	}
+	cout << min({ a[0],a[1],a[2],a[3],a[4],a[5],a[6],a[7],a[8],a[9] }) << "\n";
 }
