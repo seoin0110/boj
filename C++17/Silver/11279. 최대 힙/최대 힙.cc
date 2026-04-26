333fa92c9c08061d06c5404dc36d906729aad6c7
김서인 <79834222+seoin0110@users.noreply.github.com>
2026-04-26 17:53:24 +0900

    [Silver II] Title: 최대 힙, Time: 44 ms, Memory: 4468 KB -BaekjoonHub

---
+++ C++17/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<cmath>
+#include<queue>
+#include<string>
+#include<cstring>
+#include<stack>
+#include<map>
+#include<set>
+#include<deque>
+#include<functional>
+
+using ll = long long;
+using namespace std;
+using P = pair<ll, ll>;
+using PP = pair<ll, P>;
+const ll n_ = 1e5 + 100, inf = 1e18, mod = 1e9 + 7;
+int main() {
+	cin.tie(0), cout.tie(0);
+	ios_base::sync_with_stdio(0);
+	
+
+	ll n;
+	cin >> n;
+	vector<ll>v(n);
+	priority_queue<ll>pq;
+	for (ll i = 0; i < n;i++) {
+		cin >> v[i];
+		if (v[i] == 0) {
+			if (pq.empty())
+			{
+				cout << "0\n";
+			}
+			else
+			{
+				cout << pq.top() << '\n';
+				pq.pop();
+			}
+
+		}
+		else
+		{
+			pq.push(v[i]);
+		}
+	}
+
+
+    return 0;
+}
