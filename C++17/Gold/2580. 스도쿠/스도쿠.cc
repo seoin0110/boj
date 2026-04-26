d63757a9c94ae336ca42016b74ac714341f626d3
김서인 <79834222+seoin0110@users.noreply.github.com>
2026-04-26 17:39:20 +0900

    [Gold IV] Title: 스도쿠, Time: 216 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/Gold/2580. 스도쿠/스도쿠.cc
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <functional>
+#include <climits>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+using namespace std;
+using ll = long long;
+
+bool END;
+char s[9][9];
+void func(ll x) {
+	//cout << x / 9 << ' ' << x % 9 << '\n';
+	if (END)return;
+	if (x == 81)
+	{
+		for (ll i = 0; i < 9;i++) {
+			for (int j = 0;j < 9;j++) {
+				cout << s[i][j] << ' ';
+			}
+			cout << '\n';
+		}
+		END = true;
+		return;
+	}
+	if(s[x/9][x%9]!='0')
+		func(x + 1);
+	else {
+		for (ll i = 0;i < 9;i++) {
+			char k = i + '1';
+			bool c = true;
+			for (ll j = 0; j < 9;j++) {
+				if (s[x / 9][j] == k || s[j][x % 9] == k || s[(x / 9) / 3 * 3+ j / 3][(x % 9) / 3 *3 + j % 3] ==k)
+				{
+					c = false;
+					break;
+				}
+			}
+			
+			if (!c)	continue;
+			s[x / 9][x % 9] = k;
+			func(x + 1);
+			s[x / 9][x % 9] = '0';
+		}
+	}
+}
+
+
+int main() {
+	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	
+	for (ll i = 0; i < 9;i++) {
+		for(ll j = 0;j<9;j++)
+			cin >> s[i][j];
+	}
+	func(0);
+
+	return 0;
+}
