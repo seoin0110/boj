332394c8c2a3045a1ec919c84d80a131e366208f
김서인 <79834222+seoin0110@users.noreply.github.com>
2026-04-26 17:26:13 +0900

    [Silver IV] Title: 별 찍기 - 19, Time: 8 ms, Memory: 2176 KB -BaekjoonHub

---
+++ C++17/Silver/10994. 별 찍기 － 19/별 찍기 － 19.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+char star[400][400];
+
+
+void solution(int x, int y, int n){
+  int line = 4*(n-1);
+
+  if (n==0)
+    return;
+  for (int i=0; i<line+1; i++){
+    for (int j=0; j<line+1; j++){
+      if (i==0 || i==line){
+        star[x+i][y+j] = '*';
+        continue;
+      } 
+
+      if (j==0 || j==line) {
+        star[x+i][y+j] = '*';
+        continue;
+      }
+      star[x+i][y+j] = ' ';
+    }
+  }
+  solution(x+2, y+2, n-1);
+}
+
+int main(){
+  int n, line;
+  cin >> n;
+  solution(0, 0, n);
+
+  line = 4*(n-1);
+  for (int i=0; i<line+1; i++){
+    for (int j=0; j<line+1; j++){
+      cout << star[i][j];
+    }
+    cout << "\n";
+  }
+}
