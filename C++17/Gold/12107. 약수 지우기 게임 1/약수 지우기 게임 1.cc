20ebda79801814a8b9b87b7a322489ead0cf33d2
김서인 <79834222+seoin0110@users.noreply.github.com>
2026-04-26 17:36:36 +0900

    [Gold III] Title: 약수 지우기 게임 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/Gold/12107. 약수 지우기 게임 1/약수 지우기 게임 1.cc
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<cmath>
+#include<queue>
+#include<string>
+#include<cstring>
+#include<stack>
+#include<map>
+#include<set>
+#include<deque>
+#include<functional>
+
+using ll = long long;
+using namespace std;
+#define inf 1000000007
+int main() {
+    ll n;
+    cin >> n;
+    ll count = 0;
+    
+    if (n != 1)
+    {
+        cout << "A";
+    }
+    else
+        cout << "B";
+
+    return 0;
+}
