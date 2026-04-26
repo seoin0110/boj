97ae32dcfdf51d307f65e575d8856616547e71a1
김서인 <79834222+seoin0110@users.noreply.github.com>
2026-04-26 18:02:54 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ C++17/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0), cout.tie(0);
+	cout << fixed;
+	cout.precision(10);
+	int a, b;
+	cin >> a >> b;
+	cout << (double)a / b<<"\n";
+
+
+}
