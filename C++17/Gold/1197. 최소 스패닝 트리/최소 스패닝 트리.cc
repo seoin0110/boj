f45c711df7019ede67e083fde53ecfce4cd200e8
김서인 <79834222+seoin0110@users.noreply.github.com>
2026-04-26 17:53:04 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 32 ms, Memory: 4448 KB -BaekjoonHub

---
+++ C++17/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<cmath>
+#include<queue>
+#include<string>
+#include<cstring>
+#include<stack>
+#include<map>
+#include<set>
+#include<deque>
+#include<functional>
+
+using ll = long long;
+using namespace std;
+using P = pair<ll, ll>;
+using PP = pair<ll, P>;
+const ll n_ = 1e5 + 100, inf = 1e18, mod = 1e9 + 7;
+
+ll par[10001];
+typedef struct st {
+	ll a;
+	ll b;
+	ll c;
+};
+int find(int x) {
+	if (par[x] < 0)return x;
+	else
+		return par[x] = find(par[x]);
+}
+bool merge(int x, int y) {
+	x = find(x);
+	y = find(y);
+	if (x == y)return false;
+	else {
+		par[y] = x;
+		return true;
+	}
+}
+bool cmp(const st& s1, const st& s2) {
+	return s1.c < s2.c;
+}
+int main() {
+	cin.tie(0), cout.tie(0);
+	ios_base::sync_with_stdio(0);
+	ll v, e;
+	cin >> v >> e;
+	vector<st>tree(e);
+	for (ll i = 0; i <= v;i++) {
+		par[i] = -1;
+	}
+	for (ll i = 0;i < e;i++) {
+		ll tmp1, tmp2, tmp3;
+		cin >> tmp1 >> tmp2 >> tmp3;
+		tree[i].a = tmp1;
+		tree[i].b = tmp2;
+		tree[i].c = tmp3;
+	}
+	ll n = 0;
+	sort(tree.begin(), tree.end(), cmp);
+	ll i = 0;
+	ll sum = 0;
+	while (n < v-1 && i < e) {
+		if (merge(tree[i].a,tree[i].b)) {
+			sum += tree[i].c;
+			n++;
+		}
+		i++;
+	}
+
+	cout << sum << '\n';
+    return 0;
+}
