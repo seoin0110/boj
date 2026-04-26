405aefc0b70df8883e5d32695df5306a34abac87
김서인 <79834222+seoin0110@users.noreply.github.com>
2026-04-26 18:01:54 +0900

    [Silver III] Title: 돌 게임 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/Silver/9659. 돌 게임 5/돌 게임 5.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+using ll = long long;
+int main(){
+	ll n;
+	cin >> n;
+	if (n % 2 == 1) {
+		cout << "SK\n";
+	}
+	else {
+		cout << "CY\n";
+	}
+
+}
